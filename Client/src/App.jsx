2ff6b7c172c2e78eba3292c5a5c5c585494db047
sifaananazrin <shifananazrin15@gmail.com>
2023-04-19 16:50:03 +0530

    loading added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,29 +9,11 @@ import TeacherRouter from "./Route/TeacherRouter";
 import { BounceLoader } from "react-spinners";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+
 
   return (
     <>
-      {loading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
-          }}
-        >
-          <BounceLoader color={" #9a0eea"} loading={loading} size={150} />
-        </div>
-      ) : (
+    
         <Router>
           <Routes>
          
@@ -41,7 +23,7 @@ function App() {
             <Route path="/teacher/*" element={<TeacherRouter />} />
           </Routes>
         </Router>
-      )}
+    
     </>
   );
 }
--- Client/src/Pages/Admin/ApproveTeacher/index.jsx
+++ Client/src/Pages/Admin/ApproveTeacher/index.jsx
@@ -1,79 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@material-ui/core';
-import axios from '../../../api/axios';
-import requests from '../../../api/request';
-import swal from 'sweetalert';
-import {config} from "../../../Helpers/axiosAdminEndpoints"
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@material-ui/core";
+import axios from "../../../api/axios";
+import requests from "../../../api/request";
+import swal from "sweetalert";
+import Spinner from "../../../component/Spinner";
+import { config } from "../../../Helpers/axiosAdminEndpoints";
+import BounceLoader from "react-spinners/BounceLoader";
 
 function Teacher() {
-  // const config = {
-  //   headers: {
-  //     Authorization: `${localStorage.getItem("admintoken")}`,
-  //   },
-  // };
-
   const [teacher, setTeacher] = useState([]);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-
-      
-
-
-  //       const response = await axios.get(requests.getAllTeacher, config);
-       
-  //       setTeacher(response.data.teacher);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-
-  //   fetchData();
-  // }, [config]);
-
-  // const handleBlock =  (id) => {
-  //   swal({
-  //     title: "Are you sure?",
-  //     text: "You want to approve the teacher ",
-  //     icon: "warning",
-  //     buttons: true,
-  //     dangerMode: true,
-  //   })
-  //   .then((willDelete) => {
-  //     if (willDelete) {
-  //       swal("The teacher is teacher!", {
-  //         icon: "success",
-  //       });
-  //       block(id);
-  //     } else {
-  //       swal("The teacher not approve");
-  //     }
-  //   });
-  // };
-
-  // const block=async(id)=>{
-  //   try {
-
-
-      
-  //     const response = await axios.get(`/admin/approve/${id}`, config);
-  //     console.log(response)
-     
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
     try {
+      setLoading(true);
       const response = await axios.get(requests.getAllTeacher, config);
       setTeacher(response.data.teacher);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
-  
+
   const handleBlock = (id) => {
     swal({
       title: "Are you sure?",
@@ -92,7 +49,7 @@ function Teacher() {
       }
     });
   };
-  
+
   const block = async (id) => {
     try {
       const response = await axios.get(`/admin/approve/${id}`, config);
@@ -102,15 +59,17 @@ function Teacher() {
       console.log(error);
     }
   };
-  
+
   useEffect(() => {
+    // setLoading(true);
     fetchData();
   }, [config]);
-  
-  
-  
 
   return (
+    <>
+    {loading ? (
+      <Spinner loading={loading} />
+    ) : (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
@@ -129,23 +88,23 @@ function Teacher() {
                 <TableCell>{teachers.email}</TableCell>
                 <TableCell>{teachers.status}</TableCell>
                 <TableCell>
-                 
-                <Button
-  variant="contained"
-  color={teachers.status=="pending"? "primary" : "secondary"}
-  onClick={() => handleBlock(teachers._id)}
->
-  {teachers.status==="pending"? "approve" : "reject"}
-</Button>
-
-                  
-                 
+                  <Button
+                    variant="contained"
+                    color={
+                      teachers.status == "pending" ? "primary" : "secondary"
+                    }
+                    onClick={() => handleBlock(teachers._id)}
+                  >
+                    {teachers.status === "pending" ? "approve" : "reject"}
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
         </TableBody>
       </Table>
     </TableContainer>
+    )}
+    </>
   );
 }
 
