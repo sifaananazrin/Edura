fd3dbfba5abd3c844d1a9a39159acbaa33a9881d
Shifana nasrin <shifananazrin15@gmail.com>
2023-07-27 10:17:48 +0530

    refactor code

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance =axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://learning-platform-j1m7.onrender.com"
 });
 
 export default instance;
